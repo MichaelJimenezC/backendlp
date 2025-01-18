c4fc16cacfdeb030acdf3872c715c89f0cc13117
Geovanny Villacreses <gdvillac@espol.edu.ec>
2025-01-17 23:53:44 -0500

    cambio #?

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -24,4 +24,35 @@ class CategoryController extends Controller
 
         return response()->json($category, 201);
     }
+
+  
+
+    public function store(Request $request, $productId)
+    {
+        $request->validate([
+            'rating' => 'required|integer|min:1|max:5', 
+            'comment' => 'nullable|string|max:500', 
+        ]);
+
+        // Crear la reseña para el producto
+        $review = Review::create([
+            'user_id' => auth()->id(), 
+            'product_id' => $productId, 
+            'rating' => $request->rating, 
+            'comment' => $request->comment,
+        ]);
+
+        return response()->json(['success' => 'Reseña enviada con éxito.'], 201);
+    }
+
+    public function show($productId)
+    {
+        // Obtener las reseñas para un producto específico
+        $product = Product::with('reviews.user')->findOrFail($productId);
+
+        return response()->json($product->reviews);
+    }
 }
+
+
+
